490f48ecb8cec35db0535cc71a455e857e059d80
Hideki-SWU <h-mori@swu.ac.jp>
2025-08-11 20:49:57 +0900

    Create main.ts

---
+++ main.ts
@@ -0,0 +1,11 @@
+/**
+ * ちょびっとボード - micro:bit DCモーター制御拡張機能
+ * micro:bitでDCモーターを簡単に動かそう！
+ */
+
+//% weight=100 color=#FF6600 icon="\uf013" block="ちょびっとボード"
+//% groups=['基本操作', '詳細操作']
+namespace chobittoBoard {
+    
+    // ... 【上記で作成したmain.tsの全内容をここに貼り付け】
+}
