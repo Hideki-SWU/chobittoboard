ba7a33cf694dbe841816f23608311cac611dfa2f
Hideki-SWU <h-mori@swu.ac.jp>
2025-08-12 16:43:03 +0900

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,8 +1,5 @@
 /**
  * ちょびっとボード DCモーターテストプログラム
- */
-/**
- * Motor Control Extension Test
  * 拡張機能のテスト用プログラム
  */
 
@@ -10,8 +7,8 @@
 basic.showString("TEST")
 basic.pause(1000)
 
-// モーター制御を初期化
-motorControl.initMotorControl()
+// ちょびっとボードをはじめる
+chobittoBoard.initMotorControl()
 
 basic.forever(function () {
     // Aボタン: 基本的なモーター制御テスト
@@ -20,28 +17,28 @@ basic.forever(function () {
         
         // モーターAを段階的に速度アップ
         for (let speed = 0; speed <= 1023; speed += 200) {
-            motorControl.setMotorSpeed(MotorPin.A, speed)
+            chobittoBoard.setMotorSpeed(MotorPin.A, speed)
             basic.pause(500)
         }
         
         // モーターを停止
-        motorControl.setMotor(MotorPin.A, MotorState.Off)
+        chobittoBoard.setMotor(MotorPin.A, MotorState.Off)
         basic.showIcon(IconNames.Yes)
         basic.pause(1000)
         basic.clearScreen()
     })
 
-    // Bボタン: 複数モーター制御テスト
+    // Bボタン: 両方のモーター制御テスト
     input.onButtonPressed(Button.B, function () {
         basic.showString("B")
         
-        // すべてのモーターを順番にテスト
-        let motors = [MotorPin.A, MotorPin.B, MotorPin.C, MotorPin.D]
+        // 両方のモーターを順番にテスト
+        let motors = [MotorPin.A, MotorPin.B]
         
         for (let motor of motors) {
-            motorControl.setMotorSpeed(motor, 800)
+            chobittoBoard.setMotorSpeed(motor, 800)
             basic.pause(1000)
-            motorControl.setMotor(motor, MotorState.Off)
+            chobittoBoard.setMotor(motor, MotorState.Off)
             basic.pause(500)
         }
         
@@ -50,96 +47,54 @@ basic.forever(function () {
         basic.clearScreen()
     })
 
-    // A+Bボタン: 双方向モーター制御テスト
+    // A+Bボタン: ロボットカー動作テスト
     input.onButtonPressed(Button.AB, function () {
         basic.showString("AB")
         
-        // 正転テスト
-        motorControl.setBidirectionalMotor(
-            MotorPin.A, 
-            MotorPin.B, 
-            MotorDirection.Forward, 
-            600
-        )
+        // 前進テスト
+        chobittoBoard.setBothMotors(MotorDirection.Forward, 600)
         basic.pause(2000)
         
         // 停止
-        motorControl.setBidirectionalMotor(
-            MotorPin.A, 
-            MotorPin.B, 
-            MotorDirection.Stop, 
-            0
-        )
+        chobittoBoard.setBothMotors(MotorDirection.Stop, 0)
         basic.pause(1000)
         
-        // 逆転テスト
-        motorControl.setBidirectionalMotor(
-            MotorPin.A, 
-            MotorPin.B, 
-            MotorDirection.Reverse, 
-            600
-        )
+        // 左右個別制御テスト（右カーブ）
+        chobittoBoard.setLeftRightMotors(800, 400)
         basic.pause(2000)
         
         // 停止
-        motorControl.setBidirectionalMotor(
-            MotorPin.A, 
-            MotorPin.B, 
-            MotorDirection.Stop, 
-            0
-        )
+        chobittoBoard.stopAllMotors()
         
         basic.showIcon(IconNames.Yes)
         basic.pause(1000)
         basic.clearScreen()
     })
 
-    // 傾きセンサーでモーター制御テスト
+    // 傾きセンサーでロボットカー制御テスト
     if (input.isGesture(Gesture.TiltLeft)) {
-        motorControl.setMotorSpeed(MotorPin.A, 400)
-        motorControl.setMotorSpeed(MotorPin.B, 800)
+        // 左カーブ
+        chobittoBoard.setLeftRightMotors(400, 800)
         basic.showArrow(ArrowNames.West)
     } else if (input.isGesture(Gesture.TiltRight)) {
-        motorControl.setMotorSpeed(MotorPin.A, 800)
-        motorControl.setMotorSpeed(MotorPin.B, 400)
+        // 右カーブ
+        chobittoBoard.setLeftRightMotors(800, 400)
         basic.showArrow(ArrowNames.East)
     } else if (input.isGesture(Gesture.LogoUp)) {
-        motorControl.setMotorSpeed(MotorPin.A, 800)
-        motorControl.setMotorSpeed(MotorPin.B, 800)
+        // 前進
+        chobittoBoard.setBothMotors(MotorDirection.Forward, 800)
         basic.showArrow(ArrowNames.North)
     } else if (input.isGesture(Gesture.LogoDown)) {
-        motorControl.setBidirectionalMotor(
-            MotorPin.A, 
-            MotorPin.B, 
-            MotorDirection.Reverse, 
-            800
-        )
+        // 後退
+        chobittoBoard.setBothMotors(MotorDirection.Reverse, 800)
         basic.showArrow(ArrowNames.South)
     } else if (input.isGesture(Gesture.Shake)) {
-        motorControl.stopAllMotors()
+        // 停止
+        chobittoBoard.stopAllMotors()
         basic.showIcon(IconNames.Square)
     }
 
     basic.pause(100)
-})
-
-// サーボモーターテスト（ピンP2使用）
-let servoAngle = 90
-
-input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    basic.showString("SERVO")
-    
-    // サーボを0度から180度まで動かす
-    for (let angle = 0; angle <= 180; angle += 30) {
-        motorControl.setServoAngle(MotorPin.C, angle)
-        basic.pause(500)
-    }
-    
-    // 元の位置に戻す
-    motorControl.setServoAngle(MotorPin.C, 90)
-    basic.showIcon(IconNames.Yes)
-    basic.pause(1000)
-    basic.clearScreen()
 })
 
 // 温度センサーでファン制御テスト
@@ -151,7 +106,7 @@ basic.forever(function () {
         let fanSpeed = (temperature - 22) * 100
         fanSpeed = Math.min(fanSpeed, 1023)  // 最大速度制限
         
-        motorControl.setMotorSpeed(MotorPin.D, fanSpeed)
+        chobittoBoard.setMotorSpeed(MotorPin.A, fanSpeed)
         
         // 温度を表示
         if (temperature % 5 == 0) {  // 5秒おきに表示
@@ -161,7 +116,7 @@ basic.forever(function () {
         }
     } else {
         // 温度が低い場合、ファンを停止
-        motorControl.setMotor(MotorPin.D, MotorState.Off)
+        chobittoBoard.setMotor(MotorPin.A, MotorState.Off)
     }
     
     basic.pause(1000)
@@ -175,7 +130,7 @@ basic.forever(function () {
     let speed = Math.map(lightLevel, 0, 255, 0, 1023)
     
     if (lightLevel > 50) {  // 明るい場合のみ動作
-        motorControl.setMotorSpeed(MotorPin.B, speed)
+        chobittoBoard.setMotorSpeed(MotorPin.B, speed)
         
         // 光レベルを5段階で表示
         if (lightLevel % 10 == 0) {
@@ -185,7 +140,7 @@ basic.forever(function () {
             basic.clearScreen()
         }
     } else {
-        motorControl.setMotor(MotorPin.B, MotorState.Off)
+        chobittoBoard.setMotor(MotorPin.B, MotorState.Off)
     }
     
     basic.pause(200)
@@ -199,11 +154,11 @@ input.onPinPressed(TouchPin.P0, function () {
     let frequencies = [100, 500, 1000, 2000, 5000]
     
     for (let freq of frequencies) {
-        motorControl.setPWMFrequency(MotorPin.A, freq)
-        motorControl.setMotorSpeed(MotorPin.A, 512)
+        chobittoBoard.setPWMFrequency(MotorPin.A, freq)
+        chobittoBoard.setMotorSpeed(MotorPin.A, 512)
         basic.showNumber(freq)
         basic.pause(2000)
-        motorControl.setMotor(MotorPin.A, MotorState.Off)
+        chobittoBoard.setMotor(MotorPin.A, MotorState.Off)
         basic.pause(500)
     }
     
@@ -212,32 +167,52 @@ input.onPinPressed(TouchPin.P0, function () {
     basic.clearScreen()
 })
 
-// 連続回転サーボテスト
+// 指定時間動作テスト
 input.onPinPressed(TouchPin.P1, function () {
-    basic.showString("CONT")
+    basic.showString("TIME")
     
-    // 連続回転サーボの速度テスト
-    let speeds = [-100, -50, 0, 50, 100]
+    // 3秒間モーターを動作させる
+    chobittoBoard.runMotorForDuration(MotorPin.A, 800, 3000)
     
-    for (let speed of speeds) {
-        motorControl.setContinuousServo(MotorPin.C, speed)
-        basic.showNumber(speed)
-        basic.pause(2000)
-    }
-    
-    // 停止
-    motorControl.setContinuousServo(MotorPin.C, 0)
     basic.showIcon(IconNames.Yes)
     basic.pause(1000)
     basic.clearScreen()
 })
 
-// 指定時間動作テスト
+// 両方のモーター時間動作テスト
 input.onPinPressed(TouchPin.P2, function () {
-    basic.showString("TIME")
+    basic.showString("BOTH")
     
-    // 3秒間モーターを動作させる
-    motorControl.runMotorForDuration(MotorPin.A, 800, 3000)
+    // 両方のモーターを2秒間動作
+    chobittoBoard.runBothMotorsForDuration(700, 700, 2000)
+    
+    basic.showIcon(IconNames.Yes)
+    basic.pause(1000)
+    basic.clearScreen()
+})
+
+// ロボットカーの基本動作テスト
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.showString("CAR")
+    
+    // 前進
+    chobittoBoard.setBothMotors(MotorDirection.Forward, 600)
+    basic.pause(2000)
+    
+    // 右カーブ
+    chobittoBoard.setLeftRightMotors(800, 400)
+    basic.pause(1000)
+    
+    // 左カーブ
+    chobittoBoard.setLeftRightMotors(400, 800)
+    basic.pause(1000)
+    
+    // 後退
+    chobittoBoard.setBothMotors(MotorDirection.Reverse, 600)
+    basic.pause(1000)
+    
+    // 停止
+    chobittoBoard.stopAllMotors()
     
     basic.showIcon(IconNames.Yes)
     basic.pause(1000)
@@ -249,26 +224,53 @@ function testErrorHandling() {
     basic.showString("ERR")
     
     // 範囲外の値でテスト（自動的に制限される）
-    motorControl.setMotorSpeed(MotorPin.A, 2000)  // 1023に制限される
+    chobittoBoard.setMotorSpeed(MotorPin.A, 2000)  // 1023に制限される
     basic.pause(1000)
     
-    motorControl.setMotorSpeed(MotorPin.A, -100)  // 0に制限される
+    chobittoBoard.setMotorSpeed(MotorPin.A, -100)  // 0に制限される
     basic.pause(1000)
     
-    motorControl.setServoAngle(MotorPin.B, 200)   // 180に制限される
+    chobittoBoard.setLeftRightMotors(1500, -200)   // 範囲内に制限される
     basic.pause(1000)
     
-    motorControl.setServoAngle(MotorPin.B, -50)   // 0に制限される
-    basic.pause(1000)
+    chobittoBoard.stopAllMotors()
     
     basic.showIcon(IconNames.Yes)
     basic.pause(1000)
     basic.clearScreen()
 }
 
-// 起動時にエラーハンドリングテストを実行
-basic.pause(3000)  // 他のテストが落ち着いてから実行
+// モーター状態取得テスト
+function testMotorStatus() {
+    basic.showString("STATUS")
+    
+    // モーターAを動作
+    chobittoBoard.setMotorSpeed(MotorPin.A, 500)
+    
+    // 状態確認
+    if (chobittoBoard.isMotorRunning(MotorPin.A)) {
+        basic.showIcon(IconNames.Yes)
+        let speed = chobittoBoard.getMotorSpeed(MotorPin.A)
+        basic.showNumber(speed)
+    }
+    
+    basic.pause(2000)
+    chobittoBoard.stopAllMotors()
+    
+    // 停止状態確認
+    if (!chobittoBoard.isMotorRunning(MotorPin.A)) {
+        basic.showIcon(IconNames.No)
+    }
+    
+    basic.pause(1000)
+    basic.clearScreen()
+}
+
+// 起動時にテストを順次実行
+basic.pause(3000)  // 他の処理が落ち着いてから実行
 testErrorHandling()
+basic.pause(1000)
+testMotorStatus()
 
 // テスト完了メッセージ
 basic.showString("READY")
